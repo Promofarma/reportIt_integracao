289d8c3c551b62aca4fd4712e79cdcd0c9615145
viktorpromofarma <viktor.santos@promofarma.com.br>
2026-08-11 16:39:15 -0300

    fix: alteração do endpoint

--- app/Models/Users.php
+++ app/Models/Users.php
@@ -21,7 +21,7 @@ class Users extends Model
     public function getUsers()
     {
         $usersRegisteredIds = $this->getUsersBase()->pluck('LOGIN')->filter()->toArray();
-       
+
 
         $usersBase = $this->getEmployeesBaseQuery()
             ->whereNotIn('CPF', $usersRegisteredIds)
@@ -52,39 +52,38 @@ class Users extends Model
         );
     }
 
-     public function getEmployeesBase()
+    public function getEmployeesBase()
     {
         return $this->getEmployeesBaseQuery()->get();
     }
 
 
-public static function saveUsers($dados)
-{
-    foreach ($dados as $usuario) {
-        self::updateOrCreate(
-            ['ID_USER' => $usuario['id'] ?? $usuario['ID_USER']], 
-            [
-                'LOGIN'                => $usuario['login'] ?? null,
-                'NAME'                 => $usuario['name'] ?? null,
-                'EMAIL'                => $usuario['email'] ?? null,
-                'PERFIL'               => $usuario['perfil'] ?? 'client',
-                'ENABLE'               => $usuario['enabled'] ? 1 : 0,
-                'IS_SESMT'             => $usuario['isSESMT'] ?? 0,
-                'IS_CIPA'              => $usuario['isCIPA'] ?? 0,
-                'IS_EMPLOYEE'          => $usuario['isEmployee'] ?? 0,
-                'IS_COMMITTEE_MEMBER'  => $usuario['isCommitteeMember'] ?? 0,
-                'IS_ADMIN'             => $usuario['isAdmin'] ?? 0,
-                'EMPLOYEE_ID'          => $usuario['employeeId'] ?? null,
-                'REQUIRE_NEW_PASSWORD' => $usuario['requireNewPassword'] ?? 1,
-                'LAST_ACCESS'          => Carbon::parse($usuario['lastAccess'])->format('d-m-Y H:i:s'),
-                'INSERT_DATE_TIME'     => Carbon::parse($usuario['insertDateTime'])->format('d-m-Y H:i:s'),
-                'UPDATE_DATE_TIME'     => Carbon::parse($usuario['updateDateTime'])->format('d-m-Y H:i:s'),
-                'SSO_EXPIRE_DATE_TIME' => Carbon::parse($usuario['SSOExpireDateTime'])->format('d-m-Y H:i:s'),
-                'VALIDATION_CODE'      => $usuario['validationCode'] ?? null,
-                'SSO_KEY'              => $usuario['SSOKey'] ?? null,
-            ]
-        );
+    public static function saveUsers($dados)
+    {
+        foreach ($dados as $usuario) {
+            self::updateOrCreate(
+                ['ID_USER' => $usuario['id'] ?? $usuario['ID_USER']],
+                [
+                    'LOGIN'                => $usuario['login'] ?? null,
+                    'NAME'                 => $usuario['name'] ?? null,
+                    'EMAIL'                => $usuario['email'] ?? null,
+                    'PERFIL'               => $usuario['perfil'] ?? 'client',
+                    'ENABLE'               => $usuario['enabled'] ? 1 : 0,
+                    'IS_SESMT'             => $usuario['isSESMT'] ?? 0,
+                    'IS_CIPA'              => $usuario['isCIPA'] ?? 0,
+                    'IS_EMPLOYEE'          => $usuario['isEmployee'] ?? 0,
+                    'IS_COMMITTEE_MEMBER'  => $usuario['isCommitteeMember'] ?? 0,
+                    'IS_ADMIN'             => $usuario['isAdmin'] ?? 0,
+                    'EMPLOYEE_ID'          => $usuario['employeeId'] ?? null,
+                    'REQUIRE_NEW_PASSWORD' => $usuario['requireNewPassword'] ?? 1,
+                    'LAST_ACCESS'          => Carbon::parse($usuario['lastAccess'])->format('d-m-Y H:i:s'),
+                    'INSERT_DATE_TIME'     => Carbon::parse($usuario['insertDateTime'])->format('d-m-Y H:i:s'),
+                    'UPDATE_DATE_TIME'     => Carbon::parse($usuario['updateDateTime'])->format('d-m-Y H:i:s'),
+                    'SSO_EXPIRE_DATE_TIME' => null,
+                    'VALIDATION_CODE'      => null,
+                    'SSO_KEY'              =>  null,
+                ]
+            );
+        }
     }
-}
-
 }
